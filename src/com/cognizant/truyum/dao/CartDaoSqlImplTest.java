ccf80878cd17567daa03a68a85e5518c377fba7c
Advaid <advaidg@gmail.com>
2020-10-24 08:50:56 +0530

    Completed

--- src/com/cognizant/truyum/dao/CartDaoSqlImplTest.java
+++ src/com/cognizant/truyum/dao/CartDaoSqlImplTest.java
@@ -18,10 +18,7 @@ public class CartDaoSqlImplTest {
 		testGetAllCartItem();
 		System.out.println("TESTING REMOVE CART ITEM (USER_ID=1,MENU_ITEM=1 ");
 		testRemoveCartItem();
-		System.out.println("VERIFYING WHETHER ITEM REMOVED OR NOT BY INVOKING GETALLCARTITEM(USER_ID =2)  ");
-		testGetAllCartItem();
-		System.out.println("Completed..");
-		
+	
 	}
 	
 	public static void testAddCartItem() throws ClassNotFoundException, IOException, SQLException {
@@ -48,7 +45,7 @@ public class CartDaoSqlImplTest {
 	public static void testRemoveCartItem() throws ClassNotFoundException, IOException, SQLException, CartEmptyException {
 		CartDaoSqlImp cartDaoImpl = new CartDaoSqlImp();
 		long userId = 1;
-		System.out.println("\nBefore Delelting");
+		System.out.println("\nBefore Deleting");
 		try {
 			for(MenuItem item : cartDaoImpl.getAllCartItems(userId)) {
 				System.out.println(item);
@@ -59,7 +56,7 @@ public class CartDaoSqlImplTest {
 		}
 		long menuItemId = 1;
 		cartDaoImpl.removeCartItem(userId, menuItemId);
-		System.out.println("\nAfter Delelting");
+		System.out.println("\nAfter Deleting");
 		try {
 			for(MenuItem item : cartDaoImpl.getAllCartItems(userId)) {
 				System.out.println(item);
